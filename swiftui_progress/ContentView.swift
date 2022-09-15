6f85db6d9d9048f9283d189bda646ad0d6c6527c
Dana <dyerlanova@gmail.com>
2022-09-15 12:18:22 -0500

    Initial Commit

---
+++ swiftui_progress/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui_progress
+//
+//  Created by Dana Yerlan on 9/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui_progress/swiftui_progressApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_progressApp.swift
+//  swiftui_progress
+//
+//  Created by Dana Yerlan on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_progressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
